0dc8b3d82d7da781e6c522e8a60b05adeee87153
firdaussulaiman <firdausxwell@gmail.com>
2023-07-26 00:10:26 +0800

    CRA/Creating Components/Importing MUI/ReactRouter

---
+++ servicedesk/src/App.js
@@ -0,0 +1,17 @@
+import {BrowserRouter,Routes,Link} from 'react-router-dom';
+import './App.css';
+
+//components
+import Home from './components/Home';
+import CreateIncident from './components/CreateIncident';
+import Incident from './components/Incident';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
